dc3639329f87d68cb745a2a662064139ed2c981a
Rodrigo Burgarelli <rodrigo.burgarelli1@gmail.com>
2014-07-01 23:41:21 -0300

    tabela completa e codigo do R melhorando

--- arrumaDados.R
+++ arrumaDados.R
@@ -1,16 +1,37 @@
 #função normalizadora auxiliar para ser usada no processo calcula_tudo()
 preparaExportacao = function (final) {
-  peso = 1
+  row.names(dados) = dados$codigo
+  dados$codigo = NULL
   saida = data.frame(data=character(0),variavel=character(0),valor=numeric(0),peso=numeric(0))
-  for (i in names(final)) {
-    for (j in row.names(final)) {
-      if (i != "data") {
-        nova_linha = data.frame(data=as.character(final$data[as.numeric(j)]),variavel=i,valor=final[j,i],peso=peso)
-        saida = rbind(saida,nova_linha)
-      }
-    }
-    peso = peso+1
+  for(i in row.names(codigo)) {
+    print(i)
   }
-  return(saida)
 }
 
+calcula_acumulado = function (dados) {
+  dados$acumulado_1994 = 100
+  nomes = {}
+  nomes[1] = "X1994"
+  nomes = append(nomes,names(dados)[7:26])
+  nomes = as.numeric(gsub("X","",nomes))
+  
+  #cria as colunas acumulado_1995 e por aí vai, com o acumulado até então
+  for (coluna in seq(1,length(nomes))) {
+    if (nomes[coluna] != 1994) {
+      coluna_atual = paste("acumulado",nomes[coluna],sep="_")
+      coluna_anterior = paste("acumulado",nomes[coluna-1],sep="_")
+      coluna_valor= paste("X",nomes[coluna],sep="")    
+      dados[[coluna_atual]] = dados[[coluna_anterior]]*(1+(dados[[coluna_valor]]/100))
+    }
+  }
+  
+  #retira o valor do IPCA dessas colunas 
+  for (coluna in seq(1,length(nomes))) {
+    if (nomes[coluna] != 1994) {      
+      coluna_atual = paste("acumulado",nomes[coluna],sep="_")
+      variacao_ipca = dados[dados$codigo == "0",][[coluna_atual]]
+      dados[[coluna_atual]] = (dados[[coluna_atual]]-variacao_ipca)/variacao_ipca      
+    }
+  }
+  return(dados)
+}
